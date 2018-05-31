52245ea9d476aad7fde7fe1942b404d1ef24d9f8
Hassan Ashraf <Selv3R@Hassan-Ashrafs-MacBook-Pro.local>
2018-05-31 18:05:36 +0200

    Initial Xcode Project

---
+++ Window Shopper/Window Shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Window Shopper
+//
+//  Created by Hassan Ashraf on 5/31/18.
+//  Copyright © 2018 h2m. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
